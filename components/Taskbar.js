346caa1509909bea5d6105faf2ef5a6929c68250
AdityaChaudhary18 <sanjords@gmail.com>
2022-01-25 00:39:55 +0530

    User profile skeleton done

--- components/Taskbar.js
+++ components/Taskbar.js
@@ -1,8 +1,12 @@
 import styles from "../styles/Layout.module.scss";
 import people from "../images/people.png";
 import linkedin from "../images/linkedin.png";
-
+import phone from "../images/phone.png";
+import mail from "../images/mail.png";
 import cloud from "../images/cloud.png";
+import link from "../images/link.png";
+import language from "../images/language.png";
+import web from "../images/web.png";
 
 import Image from "next/image";
 const Taskbar = () => {
@@ -13,10 +17,10 @@ const Taskbar = () => {
         <p>President</p>
         <p>
           <b>
-            Ringover |{" "}
+            Ringover |
             <span>
               <Image src={people} alt="people" />
-            </span>{" "}
+            </span>
             101-250
           </b>
         </p>
@@ -32,7 +36,47 @@ const Taskbar = () => {
         </div>
         <hr className={styles.solid}></hr>
       </div>
-      <div className={styles.topics}></div>
+      <section className={styles.topicList}>
+        <h3>Topics</h3>
+        <div className={styles.topics}>
+          <button>Some Topic</button>
+          <button>Some Topic Name</button>
+        </div>
+      </section>
+      <section className={styles.contactList}>
+        <h3>Contacts</h3>
+        <div className={styles.contact}>
+          <Image src={mail} alt="mail" />
+          <p>j.najnudel@ringover.com</p>
+        </div>
+        <div className={styles.contact}>
+          <Image src={phone} alt="phone" />
+          <p>+33 07 55 35 23 21</p>
+        </div>
+        <div className={styles.contact}>
+          <Image src={phone} alt="phone" />
+          <p>+33 99 53 05 19 21</p>
+        </div>
+      </section>
+      <section className={styles.additionalList}>
+        <h3>Additional</h3>
+        <div className={styles.additionals}>
+          <Image src={mail} alt="mail" />
+          <p>president@ringover.com</p>
+        </div>
+        <div className={styles.additionals}>
+          <Image src={language} alt="phone" />
+          <p>Français</p>
+        </div>
+        <div className={styles.additionals}>
+          <Image src={link} alt="phone" />
+          <p>www.youtube.com/ringover</p>
+        </div>
+        <div className={styles.additionals}>
+          <Image src={web} alt="phone" />
+          <p>Indian Standard (+5:30)</p>
+        </div>
+      </section>
     </div>
   );
 };
